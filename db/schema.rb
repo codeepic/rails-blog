a357adcee87b6b28dd84ff07b8eba575a3cef57e
Xavier Glab <ksaweryglab@gmail.com>
2015-04-08 13:18:45 +0100

    added user and profile models

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150407091418) do
+ActiveRecord::Schema.define(version: 20150408121603) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -19,6 +19,24 @@ ActiveRecord::Schema.define(version: 20150407091418) do
     t.datetime "published_at"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+  end
+
+  create_table "profiles", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "name"
+    t.date     "birthday"
+    t.text     "bio"
+    t.string   "color"
+    t.string   "twitter"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email"
+    t.string   "password"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
