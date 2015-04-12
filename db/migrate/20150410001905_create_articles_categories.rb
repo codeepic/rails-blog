dc6145899168c9f59ee5d46ed301eb9a624666b8
Xavier Glab <ksaweryglab@gmail.com>
2015-04-13 00:16:25 +0100

    added rich many to many association between user and comments through article model

--- db/migrate/20150410001905_create_articles_categories.rb
+++ db/migrate/20150410001905_create_articles_categories.rb
@@ -1,8 +1,8 @@
 class CreateArticlesCategories < ActiveRecord::Migration
   def change
     create_table :articles_categories, :id => false do |t|
-      t.references :article #same as t.integer :article_id
-      t.references :category
+      t.references :article  # same as t.integer :article_id
+      t.references :category # same as t.integer :category_id
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150410001905) do
+ActiveRecord::Schema.define(version: 20150412230534) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -29,6 +29,15 @@ ActiveRecord::Schema.define(version: 20150410001905) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.integer  "article_id"
+    t.string   "name"
+    t.string   "email"
+    t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
