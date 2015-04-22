e695ddfd15231303bb002b3aa6192a342eafb67c
Xavier Glab <ksaweryglab@gmail.com>
2015-04-23 00:47:59 +0100

    updated seeds.rb file with more data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,30 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+user = User.create :email => 'mary@example.com',
+                   :password => 'guessit',
+                   :password_confirmation => 'quessit'
+Category.create [{:name => 'Programing'},
+                 {:name => 'Event'},
+                 {:name => 'Travel'},
+                 {:name => 'Music'},
+                 {:name => 'TV'}]
 
-article = Article.create :title => 'absolutely new seeded', :body => 'some text body goes here', :published_at => Date.new
-user = User.create :email => 'mary@example.com', :password => 'guessit'
-Category.create [{:name => 'Programming'},
-{:name => 'Event'},
-{:name => 'Travel'},
-{:name => 'Music'},
-{:name => 'TV'}]
+user.articles.create :title => 'Advanced Active Record', 
+                     :body => 'Models need t orelate to each other...',
+                     :published_at => Date.today
+user.articles.create :title => 'one to many associations',
+                     :body => 'one to many associations describe the pattern',
+                     :published_at => Date.today
+user.articles.create :title => 'Associations',
+                     :body => 'active record makes working with assoc easy',
+                     :published_at => Date.today
+
+#was
+# article = Article.create :title => 'absolutely new seeded', :body => 'some text body goes here', :published_at => Date.new
+# user = User.create :email => 'mary@example.com', :password => 'guessit'
+# Category.create [{:name => 'Programming'},
+# {:name => 'Event'},
+# {:name => 'Travel'},
+# {:name => 'Music'},
+# {:name => 'TV'}]
