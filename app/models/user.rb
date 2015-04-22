c99b64bea5c43d783c055bd0793bd74be25767d3
Xavier Glab <ksaweryglab@gmail.com>
2015-04-23 00:05:04 +0100

    added user authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,62 @@
+require 'digest'
+
 class User < ActiveRecord::Base
+  attr_accessor :password
+
   validates_uniqueness_of :email
   validates_length_of :email, :within => 5..50
-  validates_forat_of :email, :with => /^[^@][\w.-]+@[\w.-]+[.][a-z]{2,4}$/i  
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+.)+[a-z]{2,})\Z/i, :on => :create
+  #validates_format_of :email, :with => /^[^@][\w.-]+@[\w.-]+[.][a-z]{2,4}$/i  
+
+  validates_confirmation_of :password
+  validates_length_of :password, :within => 4..20
+  validates_presence_of :password, :if => :password_required?
 
   has_one :profile
   has_many :articles, -> { order('published_at DESC, title ASC')}, :dependent => :nullify
   has_many :replies, :through => :articles, :source => :comments
+
+  before_save :encrypt_new_password
+
+  def self.authenticate(email, password)
+    user = find_by_email(email)
+    return user if user && user.authenticated?(password)
+  end
+
+  def authenticated?(password)
+    self.hashed_password == encrypt(password)
+  end
+
+  protected
+    def encrypt_new_password
+      return if password.blank?
+      self.hashed_password = encrypt(password)
+    end
+
+    def password_required?
+      hashed_password.blank? || password.present?
+    end
+
+    def encrypt(string)
+      Digest::SHA1.hexdigest(string)
+    end
 end
+
+# The SHA1 hashing algorithm used in this example is weak 
+# and was only used for an example. For production web sites
+# you should take a look at the bcrypt gem
+# (https://github.com/codahale/bcrypt-ruby).
+
+
+
+
+#orig
+# class User < ActiveRecord::Base
+#   validates_uniqueness_of :email
+#   validates_length_of :email, :within => 5..50
+#   validates_forat_of :email, :with => /^[^@][\w.-]+@[\w.-]+[.][a-z]{2,4}$/i  
+
+#   has_one :profile
+#   has_many :articles, -> { order('published_at DESC, title ASC')}, :dependent => :nullify
+#   has_many :replies, :through => :articles, :source => :comments
+# end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150412230534) do
+ActiveRecord::Schema.define(version: 20150421065649) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -55,9 +55,9 @@ ActiveRecord::Schema.define(version: 20150412230534) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email"
-    t.string   "password"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "hashed_password"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
