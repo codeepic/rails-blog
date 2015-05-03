0776776693e2102c4bb5f87b3b3927b9f80ae07c
Xavier Glab <ksaweryglab@gmail.com>
2015-05-03 14:11:36 +0100

    cleaned up layout, added form action buttons application helper,

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  #creates a submit button with the given name with a cancel link
+  #accepts 2 arguments: form object and the canel link name
+  def submit_or_cancel(form, name='Cancel')
+    form.submit + " or " + link_to(name, 'javascript:history.go(-1);', :class => 'cancel')
+  end
 end
