3cd6a5c6c375309efa602e6b6a23756202188b0e
Xavier Glab <ksaweryglab@gmail.com>
2015-05-05 00:23:59 +0100

    added few english localisations to test I18n module

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -5,7 +5,8 @@ class Comment < ActiveRecord::Base
   validate :article_should_be_published
 
   def article_should_be_published
-    errors.add(:article_id, 'is not published yet') if article && !article.published?
+    # errors.add(:article_id, 'is not published yet') if article && !article.published?
+    errors.add(:article_id, I18n.t('comments.errors.not_published_yet')) if article && !article.published?
   end
 
   after_create :email_article_author
