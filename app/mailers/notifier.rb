3146bd4570d247f58300fca6efd1503489494117
Xavier Glab <ksaweryglab@gmail.com>
2015-05-04 12:50:33 +0100

    added email a friend functionality under articles

---
+++ app/mailers/notifier.rb
@@ -0,0 +1,17 @@
+class Notifier < ApplicationMailer
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.notifier.email_friend.subject
+  #
+  def email_friend(article, sender_name, receiver_email)
+    @article = article
+    @sender_name = sender_name
+
+    mail :to => receiver_email, :subject => 'Interesting article'
+
+    # @greeting = "Hi"
+    # mail to: "xavier.glab@channelforge.com"
+  end
+end
